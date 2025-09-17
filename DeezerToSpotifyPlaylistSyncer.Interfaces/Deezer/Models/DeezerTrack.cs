82ebb61007f2738253ffa5384631ebd0abe35149
Florian Mazzeo <40074382+Scryper@users.noreply.github.com>
2025-09-17 19:49:18 +0200

    Fix fetching tracks with featurings (#6)

--- DeezerToSpotifyPlaylistSyncer.Interfaces/Deezer/Models/DeezerTrack.cs
+++ DeezerToSpotifyPlaylistSyncer.Interfaces/Deezer/Models/DeezerTrack.cs
@@ -2,6 +2,8 @@
 
 public class DeezerTrack
 {
+	public required long Id { get; set; }
 	public required string Title { get; set; }
 	public required DeezerArtist Artist { get; set; }
+	public IEnumerable<DeezerArtist>? Contributors { get; set; }
 }
