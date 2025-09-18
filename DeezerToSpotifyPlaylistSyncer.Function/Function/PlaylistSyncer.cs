f0f933c13a1ca311da5511859ee634f018f6a388
Florian Mazzeo <40074382+Scryper@users.noreply.github.com>
2025-09-18 08:18:27 +0200

    Update PlaylistSyncer.cs

--- DeezerToSpotifyPlaylistSyncer.Function/Function/PlaylistSyncer.cs
+++ DeezerToSpotifyPlaylistSyncer.Function/Function/PlaylistSyncer.cs
@@ -14,26 +14,31 @@ public class PlaylistSyncer(ILogger<PlaylistSyncer> logger, IDeezerPlaylistServi
 	[Function(nameof(PlaylistSyncer))]
 	public async Task RunAsync([TimerTrigger("0 0 */1 * * *")] TimerInfo myTimer)
 	{
-		this._logger.LogWarning("Playlist syncer started");
-		var deezerPlaylist = await this._deezerPlaylistService.GetPlaylistAsync();
-		if (deezerPlaylist?.Tracks is null)
-		{
-			this._logger.LogError("Fatal error: deezer playlist not found");
-			return;
+		try {
+			this._logger.LogWarning("Playlist syncer started");
+			var deezerPlaylist = await this._deezerPlaylistService.GetPlaylistAsync();
+			if (deezerPlaylist?.Tracks is null)
+			{
+				this._logger.LogError("Fatal error: deezer playlist not found");
+				return;
+			}
+	
+			var detailedDeezerTracks = await this._deezerPlaylistService.GetDetailedTracksAsync(deezerPlaylist.Tracks.Data.Select(data => data.Id));
+			var spotifyTracks = await this._spotifyPlaylistService.GetTrackIdsAsync(detailedDeezerTracks);
+			var spotifyPlaylist = await this._spotifyPlaylistService.GetPlaylistAsync();
+			if (spotifyPlaylist is null)
+			{
+				this._logger.LogError("Fatal error: spotify playlist not found");
+				return;
+			}
+	
+			await this._spotifyPlaylistService.AddMissingTracksAsync(spotifyPlaylist, spotifyTracks);
+			await this._spotifyPlaylistService.RemoveOldTracksAsync(spotifyPlaylist, spotifyTracks);
+	
+			this._logger.LogWarning("Playlist syncer ended");
+		} catch(Exception e) {
+			this._logger.LogError(e, "PlaylistSyncer failed with exception: {message}", e.Message);
+        	throw;
 		}
-
-		var detailedDeezerTracks = await this._deezerPlaylistService.GetDetailedTracksAsync(deezerPlaylist.Tracks.Data.Select(data => data.Id));
-		var spotifyTracks = await this._spotifyPlaylistService.GetTrackIdsAsync(detailedDeezerTracks);
-		var spotifyPlaylist = await this._spotifyPlaylistService.GetPlaylistAsync();
-		if (spotifyPlaylist is null)
-		{
-			this._logger.LogError("Fatal error: spotify playlist not found");
-			return;
-		}
-
-		await this._spotifyPlaylistService.AddMissingTracksAsync(spotifyPlaylist, spotifyTracks);
-		await this._spotifyPlaylistService.RemoveOldTracksAsync(spotifyPlaylist, spotifyTracks);
-
-		this._logger.LogWarning("Playlist syncer ended");
 	}
 }
